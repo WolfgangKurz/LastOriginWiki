c4500d90f99ac25084a1968a4f7df87a4cffd3f1
WolfgangKurz <wolfgangkurzdev@gmail.com>
2020-09-22 16:07:11 +0900

    스토리 파서 완성 (1차)

--- src/pages/Story/StoryViewer.vue
+++ src/pages/Story/StoryViewer.vue
@@ -4,9 +4,17 @@ import Component from "vue-class-component";
 import { Watch } from "vue-property-decorator";
 
 import { StoryRaw } from "@/libs/Types";
-import { StoryData } from "@/libs/DB";
+import { MapData, StoryData, UnitUid } from "@/libs/DB";
+import { AssetsRoot, WorldNames } from "@/libs/Const";
+import { UpdateTitle } from "@/libs/Functions";
 
-@Component({})
+import UnitFace from "@/components/UnitFace.vue";
+
+@Component({
+	components: {
+		UnitFace,
+	},
+})
 export default class StoryViewer extends Vue {
 	private readonly BaseURL = "https://lastorigin-wiki-assets.s3.ap-northeast-2.amazonaws.com/story/";
 
@@ -49,6 +57,16 @@ export default class StoryViewer extends Vue {
 		this.spec = params.spec;
 
 		this.loadStory();
+	}
+
+	private get Name () {
+		return WorldNames[this.world] || this.world;
+	}
+
+	private get Area () {
+		if (this.world in MapData && this.area in MapData[this.world])
+			return MapData[this.world][this.area].title;
+		return "???";
 	}
 
 	private get Story (): StoryRaw | null {
@@ -104,48 +122,144 @@ export default class StoryViewer extends Vue {
 			const parsed = dom.parseFromString(`<x>${line}</x>`, "text/xml");
 
 			let teller = "";
+			let tellerId = 0;
+			const selects: Array<{
+				text: string;
+				to: string;
+			}> = [];
 
-			parsed.childNodes[0].childNodes.forEach(node => {
-				if (node.nodeType === Node.COMMENT_NODE)
-					return;
-				else if (node.nodeType === Node.TEXT_NODE) {
+			parsed.childNodes[0].childNodes.forEach(_node => {
+				if (_node.nodeType === Node.COMMENT_NODE) return;
+
+				if (_node.nodeType === Node.TEXT_NODE) {
+					const node = _node as Text;
+
 					if (node.textContent?.trim()) {
-						if (teller) {
-							ret.push(<div class="story-dialogue">
-								{teller}:
-								{node.textContent.split("\n").map(x => <div>{x}</div>)}
-							</div>);
-						} else {
-							ret.push(<div class="story-dialogue">
-								{node.textContent.split("\n").map(x => <div>{x}</div>)}
-							</div>);
-						}
-					} else
-						return;
+						ret.push(<div class="story-dialogue">
+							<div class="story-dialogue-teller">
+								<unit-face id={tellerId} size="60" />
+								{teller}
+							</div>
+							<div class="story-dialogue-text">
+								{
+									node.textContent
+										.split("\n")
+										.filter(x => x)
+										.map(x => <div>{
+											x.split(/(\$player)|(\[\[[^\]]+\]\])/g)
+												.filter(y => typeof y === "string")
+												.map(y => {
+													if (y === "$player")
+														return <b-badge class="badge-text" variant="light">사령관</b-badge>;
+													else if (y.startsWith("[["))
+														return <span class="text-warning">{y.substring(2, y.length - 2)}</span>;
+													else
+														return y;
+												})
+										}</div>)
+								}
+							</div>
+						</div>);
+					}
 				} else {
+					const node = _node as Element;
 					switch (node.nodeName) {
 						case "title":
-							ret.push(<h2 class="text-center">{plain(node)}</h2>);
+							ret.push(<h1 class="text-center">{plain(node)}</h1>);
 							break;
 						case "loc":
-							ret.push(<h4 class="text-center">
+							ret.push(<h3 class="text-center">
 								<b-badge variant="dark">{plain(node)}</b-badge>
-							</h4>);
+							</h3>);
 							break;
 						case "teller":
-							teller = plain(node);
+							{
+								teller = plain(node);
+
+								const face = node.getAttribute("face");
+								if (face) {
+									const unit = Object.keys(UnitUid)
+										.map(x => {
+											const id = parseInt(x, 10);
+											return { key: id, value: UnitUid[id] };
+										})
+										.find(x => x.value === face);
+
+									if (!unit)
+										tellerId = 0;
+									else
+										tellerId = unit.key;
+								}
+							}
+							break;
+						case "comment":
+							ret.push(<b-alert class="mx-4 text-center" variant="success" show>{plain(node)}</b-alert>);
+							break;
+						case "scene":
+							{
+								const bg = node.getAttribute("bg");
+								if (bg) {
+									ret.push(<div class="story-scene text-center">
+										<img src={`${AssetsRoot}/story/${bg}.jpg`} />
+									</div>);
+								}
+							}
+							break;
+						case "select":
+							{
+								const content = plain(node);
+								const to = node.getAttribute("to");
+								if (!to) return;
+
+								selects.push({ text: content, to });
+							}
+							break;
+						case "flag":
+							ret.push(<div class="text-right text-warning pr-4" id={node.id}>
+								<b-icon-flag-fill class="mr-2" />선택지 도착점
+							</div>);
 							break;
 					}
 				}
-				ret.push(<hr />);
 			});
+
+			if (selects.length > 0) {
+				ret.push(<div class="text-center">
+					{selects.map(x => <b-button
+						class="mx-1"
+						variant="warning"
+						onClick={() => this.ScrollTo(x.to)}
+					>{x.text}</b-button>)}
+				</div>);
+			}
+
+			ret.push(<hr />);
 		});
 
 		return ret;
 	}
 
+	private ScrollTo (id: string) {
+		const target = this.$el.querySelector<HTMLDivElement>(`#${id}`);
+		if (!target) return;
+
+		let y = 0;
+		let root: Element | null = target;
+		while (root && root instanceof HTMLElement && root.offsetParent !== root) {
+			y += root.offsetTop;
+			root = root.offsetParent;
+		}
+
+		window.scrollTo({ top: y });
+	}
+
 	private mounted () {
 		this.checkParams();
+
+		if (!this.Story)
+			UpdateTitle(["이야기", this.Name, this.Area]);
+		else
+			UpdateTitle(["이야기", this.Name, this.Area, `[${this.Story.map}-${this.Story.loc}]`, this.Story.title]);
 	}
 
 	private render () {
@@ -159,39 +273,81 @@ export default class StoryViewer extends Vue {
 			</b-row>
 			<hr />
 
-			{
-				this.Story
-					? [
-						<b-alert variant="dark" show>
-							<h4 class="m-0">
-								<b-badge class="mr-3" variant="dark">{this.Story.map}-{this.Story.loc}</b-badge>
-								{this.Story.title}
-							</h4>
-						</b-alert>,
-						<hr />,
-					]
-					: []
-			}
+			<div class="story-box bg-dark text-light">
+				{
+					this.Story
+						? [
+							<b-alert class="mb-2" variant="light" show>
+								<h4 class="m-0">
+									<b-badge class="mr-3" variant="dark">{this.Story.map}-{this.Story.loc}</b-badge>
+									{this.Story.title}
+								</h4>
+							</b-alert>,
+							<hr />,
+						]
+						: []
+				}
 
-			{
-				this.storyData.length > 0
-					? this.Parsed
-					: <h3 class="text-center">
-						<b-icon-hourglass-split class="mr-2" />
-						이야기 데이터를 불러오고 있습니다
-					</h3>
-			}
-		</div >;
+				{
+					this.storyData.length > 0
+						? this.Parsed
+						: <h3 class="text-center">
+							<b-icon-hourglass-split class="mr-2" />
+							이야기 데이터를 불러오고 있습니다
+						</h3>
+				}
+			</div>
+		</div>;
 	}
 }
 </script>
 
 <style lang="scss">
 .story-viewer {
-	.story-dialogue {
-		padding: 0.25rem;
-		background-color: #222;
-		color: #fff;
+	.story-box {
+		padding: 0.5rem;
+
+		hr {
+			border-top-color: rgba($white, 0.1);
+		}
+
+		.story-dialogue {
+			display: flex;
+			flex-direction: row;
+			padding: 0.25rem;
+
+			> .story-dialogue-teller {
+				display: flex;
+				margin-right: 1rem;
+				min-width: 100px;
+				flex-direction: column;
+				word-break: keep-all;
+				align-items: center;
+
+				word-spacing: 1px;
+				font-weight: bold;
+				font-size: 80%;
+
+				> img {
+					margin-right: 0.25rem;
+				}
+			}
+			> .story-dialogue-text {
+				flex: 1;
+				word-break: keep-all;
+
+				> div:not(:first-child) {
+					margin-top: 0.5rem;
+				}
+
+				.badge.badge-text {
+					font-size: 100%;
+				}
+			}
+		}
+		.story-scene > img {
+			max-width: 100%;
+		}
 	}
 }
 </style>
