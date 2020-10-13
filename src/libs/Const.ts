9828d1097442e69004f797e3981749d44edfcc7a
Wolfgang Kurz <wolfgangkurzdev@gmail.com>
2020-10-13 21:57:43 +0900

    세계 설명 통합

--- src/libs/Const.ts
+++ src/libs/Const.ts
@@ -58,3 +58,14 @@ export const WorldNames: Record<string, string> = {
 	FairyAria: "요정 마을의 아리아",
 	BlurryMemory: "흐린 기억 속의 나라",
 };
+export const WorldDescriptions: Record<string, string> = {
+	Story: "",
+	SupremeDinner: "소완으로 인해 혼란에 빠진 오르카 안에선 무슨 일이?",
+	Rioboros: "리오보로스의 유산을 찾아 떠나는 모험!",
+	FullMoonNocturne: "마법 소녀들과 함께 뽀끄루 대마왕을 물리쳐주세요!",
+	HalloweenPanic: "과거에 얽매이지 마. 지금을 즐기는 거야!",
+	SaintOrca: "연말 파티를 준비하는 사령관에게 뻗쳐오는 마수는?",
+	ChocolateQueen: "티아멧은 '초코 여왕'과 얽힌 멸망 전 인류의 일그러짐과 마주하는데..",
+	FairyAria: "가벼운 마음으로 요정 마을로 향한 사령관에게...?",
+	BlurryMemory: "왓슨, 드디어 대단원이야!",
+};
