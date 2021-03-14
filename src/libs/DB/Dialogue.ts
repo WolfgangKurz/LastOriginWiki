55798ceef9a15dc11b0fd5177644d7075af8f070
Wolfgang Kurz <wolfgangkurzdev@gmail.com>
2021-03-14 18:51:02 +0900

    .

--- src/libs/DB/Dialogue.ts
+++
@@ -1,9 +0,0 @@
-import LoadDBFactory from "../DB/DBLoader";
-import { RawUnitDialogue } from "@/libs/Types/Dialogue";
-
-export default (locale: string, callback?: (data: RawUnitDialogue | null) => void) => {
-	return LoadDBFactory<RawUnitDialogue>(
-		`dialogue-${locale}`,
-		import(/* webpackChunkName: "chunk-db-dialogue-" */ `@/json/dialogue/${locale}`),
-	)(callback);
-};
