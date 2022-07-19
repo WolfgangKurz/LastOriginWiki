41e634d11f10a9fa1a6e5ee4b9808ea8ef796727
Wolfgang Kurz <wolfgangkurzdev@gmail.com>
2022-07-20 03:42:01 +0900

    2022-07-18 업데이트 반영

--- src/components/skill-description/components/Value.tsx
+++ src/components/skill-description/components/Value.tsx
@@ -4,33 +4,52 @@ import Decimal from "decimal.js";
 import style from "./style.module.scss";
 
 interface ValueProps {
-	base: number;
-	per: number;
+	base?: number;
+	per?: number;
 	level: number;
 	ratio?: boolean;
 	ratio2?: boolean;
 	invert?: boolean;
 	signless?: boolean;
 	floor?: boolean;
+	forcePN?: "p" | "n";
 }
 
 export const Value: FunctionalComponent<ValueProps> = (props) => {
-	let v = Decimal.add(props.base, Decimal.mul(props.per, props.level))
-		.mul(props.ratio2 ? 10000 : props.ratio ? 100 : 1);
+	if (props.base !== undefined && props.per !== undefined) {
+		let v = Decimal.add(props.base, Decimal.mul(props.per, props.level))
+			.mul(props.ratio2 ? 10000 : props.ratio ? 100 : 1);
 
-	const positive = v.isPositive();
-	const sign = props.invert ? !positive : positive;
+		const positive = v.isPositive();
+		const sign = props.invert ? !positive : positive;
 
-	const signFlag = v.isZero()
-		? ""
-		: sign ? "+" : "-";
+		const signFlag = v.isZero()
+			? ""
+			: sign ? "+" : "-";
 
-	v = v.abs();
-	if (props.floor) v = v.floor();
+		v = v.abs();
+		if (props.floor) v = v.floor();
 
-	return <span class={ style.Value } data-sign={ signFlag }>
-		{ !props.signless ? signFlag : <></> }
-		{ v.toFixed(10).replace(/\.?0+$/, "") }
-		{ (props.ratio || props.ratio2) && "%" }
-	</span>;
+		if (
+			(props.forcePN === "p" && v.isNegative()) ||
+			(props.forcePN === "n" && v.isPositive())
+		)
+			v = v.neg();
+
+		return <span class={ style.Value } data-sign={ signFlag }>
+			{ !props.signless ? signFlag : <></> }
+			{ v.toFixed(10).replace(/\.?0+$/, "") }
+			{ (props.ratio || props.ratio2) && "%" }
+		</span>;
+	} else {
+		if (props.forcePN) {
+			const signFlag = props.forcePN === "p" ? "+" : "-";
+
+			return <span class={ style.Value } data-sign={ signFlag }>
+				{ !props.signless ? signFlag : <></> }
+			</span>;
+		}
+
+		return <span class={ style.Value } data-sign="" />;
+	}
 };
