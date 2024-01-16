9f625ec91da23dcf10c6ee73703234152b6fa3a5
Wolfgang Kurz <wolfgangkurzdev@gmail.com>
2024-01-16 23:58:41 +0900

    2014-01-16

--- src/libs/Const.ts
+++ src/libs/Const.ts
@@ -26,6 +26,6 @@ export const SubStoryUnit: Record<string, string> = {
 
 export const PermanentEvents: string[] = ["Ev1", "Ev2", "Ev3", "Ev4", "Ev5", "Ev6", "Ev7", "Ev8", "Ev9"];
 export const NewMapList: string[] = [];
-export const CurrentEvent = "Ev22";
+export const CurrentEvent = "";
 export const EventFrom = new Date(2023, 12 - 1, 19);
 export const EventTo = new Date(2024, 1 - 1, 16);
