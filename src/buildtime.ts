df7aa0df1b2dc94d01d918579c8b9c6afcb11c72
Wolfgang Kurz <wolfgangkurzdev@gmail.com>
2024-02-14 04:17:14 +0900

    2024-02-13

--- src/buildtime.ts
+++ src/buildtime.ts
@@ -1,2 +1,2 @@
 // eslint-disable-next-line
-export default {"time":1707513247573,"build":12469}
+export default {"time":1707851688355,"build":12470}
--- src/routes/changelog/changelog/2024/202402.tsx
+++ src/routes/changelog/changelog/2024/202402.tsx
@@ -5,6 +5,19 @@ import { BY, BR, BO, BB, BG } from "../../components/badges";
 import IconGithub from "@/components/bootstrap-icon/icons/Github";
 
 const Changelog: FunctionalComponent = () => <>
+	<ChangelogItem title="Build 12470" date="2024-02-14"
+		skin={ <>
+			<li>
+				<BY>성벽의 하치코</BY>의 스킨 <BY>상쾌한 달콤함</BY>의 정보가 추가되었습니다.
+			</li>
+			<li>
+				<BY>화롯가의 포티아</BY>의 스킨 <BY>사령관님 전용 디저트</BY>의 정보가 추가되었습니다.
+			</li>
+			<li>
+				<BY>다프네</BY>의 스킨 <BY>다프네 인 더 키친</BY>의 정보가 추가되었습니다.
+			</li>
+		</> }
+	/>
 	<ChangelogItem title="Build 12469" date="2024-02-10"
 		site={ <>
 			<li>
