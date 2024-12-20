533338152a8b70eb6bd29187f5c5e11bedee2d29
Wolfgang Kurz <wolfgangkurzdev@gmail.com>
2024-12-20 20:32:13 +0900

    #130 fix, Unit source from story node fix

--- src/routes/story/index.tsx
+++ src/routes/story/index.tsx
@@ -70,19 +70,36 @@ const Story: FunctionalComponent<StoryProps> = (props) => {
 		11,
 		"Ev20", "Ev21", "Ev23",
 		12,
-		"Ev24", "Ev25", "Ev26", "Ev27",
+		"Ev24", "Ev25", "Ev26", "Ev27", "Ev28",
 		13,
 	];
-	const StoryList: StoryKeyType[] = useMemo(
-		() => StoryListSource.map(r => {
-			if (Array.isArray(r)) {
-				const s = r[1];
-				if (Array.isArray(s)) return [r[0], s];
-				return [r[0], [s]];
+	const StoryList: Array<StoryKeyType | null> = useMemo(
+		() => {
+			const arr: StoryKeyType[] = StoryListSource.map(r => {
+				if (Array.isArray(r)) {
+					const s = r[1];
+					if (Array.isArray(s)) return [r[0], s];
+					return [r[0], [s]];
+				}
+				return [r, [0]];
+			});
+
+			if (StoryMapDB) {
+				const unordered: StoryKeyType[] = Object.keys(StoryMapDB)
+					.filter(k => !arr.some(r => r[0] == k))
+					.map(k => [k, [0]]);
+
+				if (unordered.length > 0) {
+					return [
+						...arr,
+						null,
+						...unordered,
+					];
+				}
 			}
-			return [r, [0]];
-		}),
-		[StoryListSource],
+			return arr;
+		},
+		[StoryMapDB, StoryListSource],
 	);
 
 	function GetWorldIcon (k: number | string, s: number): string {
@@ -128,6 +145,54 @@ const Story: FunctionalComponent<StoryProps> = (props) => {
 		return onInvalid();
 	}
 
+	function StoryTimeline (k: StoryKeyType): preact.VNode[] {
+		return k[1].map(s => <div class={ style.StoryMapItem }>
+			<Link
+				class={ style.Box }
+				href={ `/story/${k[0]}_${s}` }
+			>
+				<img src={ `${AssetsRoot}/world/icons/${GetWorldIcon(k[0], s)}.png` } />
+
+				<div class={ style.StoryType }>
+					{ typeof k[0] === "number"
+						? <>
+							<span class="badge bg-warning text-bg-warning mx-1">
+								<Locale k="STORY_TYPE_MAINSTORY" />
+							</span>
+
+							<span class="badge bg-dark text-bg-dark mx-1">
+								<Locale k="WORLDS_WORLD_TITLE" p={ [k[0]] } />
+							</span>
+						</>
+						: <>
+							<span class="badge bg-danger text-bg-danger mx-1">
+								<Locale k="STORY_TYPE_EVENT" />
+							</span>
+
+							{ s !== 0 && <span class="badge bg-dark text-bg-dark mx-1">
+								<Locale k="WORLDS_WORLD_TITLE" p={ [s] } />
+							</span> }
+						</>
+					}
+				</div>
+				<div class={ style.StoryName }>
+					{ GetWorldLocale(k[0], s) }
+				</div>
+			</Link>
+		</div>);
+	}
+
+	const StoryListA = useMemo((): StoryKeyType[] => {
+		const idx = StoryList.indexOf(null);
+		if (idx >= 0) return StoryList.slice(0, idx) as StoryKeyType[];
+		return StoryList as StoryKeyType[];
+	}, [StoryList]);
+	const StoryListB = useMemo((): StoryKeyType[] => {
+		const idx = StoryList.indexOf(null);
+		if (idx >= 0) return StoryList.slice(idx + 1) as StoryKeyType[];
+		return [];
+	}, [StoryList]);
+
 	return <div class={ style.Story }>
 		<h2>
 			<Locale k="STORY" />
@@ -136,43 +201,14 @@ const Story: FunctionalComponent<StoryProps> = (props) => {
 		<hr />
 
 		<div class={ style.Timeline }>
-			{ StoryList.map(k =>
-				k[1].map(s => <div class={ style.StoryMapItem }>
-					<Link
-						class={ style.Box }
-						href={ `/story/${k[0]}_${s}` }
-					>
-						<img src={ `${AssetsRoot}/world/icons/${GetWorldIcon(k[0], s)}.png` } />
-
-						<div class={ style.StoryType }>
-							{ typeof k[0] === "number"
-								? <>
-									<span class="badge bg-warning text-bg-warning mx-1">
-										<Locale k="STORY_TYPE_MAINSTORY" />
-									</span>
-
-									<span class="badge bg-dark text-bg-dark mx-1">
-										<Locale k="WORLDS_WORLD_TITLE" p={ [k[0]] } />
-									</span>
-								</>
-								: <>
-									<span class="badge bg-danger text-bg-danger mx-1">
-										<Locale k="STORY_TYPE_EVENT" />
-									</span>
-
-									{ s !== 0 && <span class="badge bg-dark text-bg-dark mx-1">
-										<Locale k="WORLDS_WORLD_TITLE" p={ [s] } />
-									</span> }
-								</>
-							}
-						</div>
-						<div class={ style.StoryName }>
-							{ GetWorldLocale(k[0], s) }
-						</div>
-					</Link>
-				</div>)
-			) }
+			{ StoryListA.map(k => StoryTimeline(k!)) }
 		</div>
+		{ StoryListB.length > 0 && <>
+			<hr />
+			<div class={ style.Timeline }>
+				{ StoryListB.map(k => StoryTimeline(k!)) }
+			</div>
+		</> }
 
 		<PopupBase
 			display={ !!selectedKey }
