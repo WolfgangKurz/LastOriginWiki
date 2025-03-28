3e7fcd826ce71d4deb705031d433ab9acadbd62a
Wolfgang Kurz <wolfgangkurzdev@gmail.com>
2025-03-28 23:28:10 +0900

    Skin Banner

--- src/libs/Const.2.ts
+++ src/libs/Const.2.ts
@@ -127,6 +127,7 @@ export const SkinBanners: string[] = [
 	"AGS_Aeda_1",
 	"PECS_Sunny_2", "3P_Calypso_1",
 	"3P_Alcyone_2", "3P_Maria_3",
+	"PECS_LemonadeEpsilon_1", "BR_Lindwurm_3",
 ];
 
 type SkillVideo_Plain = string[];
