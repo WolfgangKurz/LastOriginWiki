6d2a1c93f324c37bcdcb6baa94e54222aa0c0c8e
Wolfgang Kurz <wolfgangkurzdev@gmail.com>
2025-10-01 01:02:46 +0900

    #171, #172

--- src/routes/units/components/skin-view/index.tsx
+++ src/routes/units/components/skin-view/index.tsx
@@ -419,7 +419,7 @@ const SkinView: FunctionalComponent<SkinViewProps> = (props) => {
 					}
 				</div>
 
-				{ modelVideoId && !!props.animate
+				{ modelVideoId && !(DisplaySpine || DisplayMixed) && !!props.animate
 					? <a
 						class={ `${style.SkinToggle} ${style.Download}` }
 						href={ `${AssetsRoot}/webm/HD/${modelVideoId}.webm` } // download webm only
