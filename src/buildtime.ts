e2aa4b10659bde48ce1b20e1b41edc125bd6f029
Wolfgang Kurz <wolfgangkurzdev@gmail.com>
2026-01-04 01:45:55 +0900

    2026-01-02

--- src/buildtime.ts
+++ src/buildtime.ts
@@ -1,2 +1,2 @@
 // eslint-disable-next-line
-export default {"time":1766358118178,"build":13044}
+export default {"time":1767438834819,"build":13047}
---
+++ src/routes/changelog/changelog/2026/202601.tsx
@@ -0,0 +1,33 @@
+import { FunctionalComponent } from "preact";
+
+import { AssetsRoot } from "@/libs/Const";
+
+import ChangelogItem from "../../components/changelog-item";
+import { BY, BR, BO, BB, BG, BP } from "../../components/badges";
+
+import Locale from "@/components/locale";
+
+const Changelog: FunctionalComponent = () => <>
+	<ChangelogItem title="Build 13047" date="2026-01-03"
+		new={ <>
+			<li>이벤트 <BY>오르카 크리스마스 엑스포</BY>의 2부 정보가 추가되었습니다.</li>
+			<li>신규 스토리 정보가 추가되었습니다.</li>
+			<li>신규 배경 정보가 추가되었습니다.</li>
+			<li>신규 장식품 정보가 추가되었습니다.</li>
+			<li>신규 소모품 정보가 추가되었습니다.</li>
+		</> }
+		knownissue={ <>
+			<li>일부 전투원의 대사 정보가 잘못 갱신되었습니다. (인게임에서 잘못되어 있습니다.)</li>
+		</> }
+		update={ <>
+			<li>일부 적의 정보가 갱신되었습니다.</li>
+		</> }
+		skin={ <>
+			<li>전투원 <BY>S7 데스스토커</BY>의 스킨 <BR>언더그라운드 리버 메탈​ : 강화 전투 모델</BR>의 정보가 추가되었습니다.</li>
+		</> }
+		dialogue={ <>
+			<li><span class="badge bg-light text-dark">KST 2026-01-03 19:51:55</span>까지 추가/수정된 대사들이 반영되었습니다.</li>
+		</> }
+	/>
+</>;
+export default Changelog;
