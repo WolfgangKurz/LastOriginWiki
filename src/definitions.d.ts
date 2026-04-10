c046224e05109f172b3c8bf2c2f9ff3d88539c1a
Wolfgang Kurz <wolfgangkurzdev@gmail.com>
2026-04-10 22:11:29 +0900

    opentype.js use yarn package

--- src/definitions.d.ts
+++ src/definitions.d.ts
@@ -3,5 +3,3 @@ import * as preactHooks from "preact/hooks";
 declare module "react" { // react type-guard
 	export type CanvasHTMLAttributes<T extends HTMLCanvasElement> = HTMLAttributes<T>;
 }
-
-declare module "opentype.js" { }
